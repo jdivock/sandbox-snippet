7e610baf8349b17e13af2f028f2c15e8aef811e2
HUGE | Jason Divock <jdivock@hugeinc.com>
2014-01-27 15:35:09 -0500

    yep, percentage based parallax happening

--- app/slideshow/main.js
+++ app/slideshow/main.js
@@ -11,15 +11,30 @@
         });
 
         window.onload = function() {
-            $('section').height($(window).innerHeight());
+            $('section').height($(window).innerHeight() * 2);
             s.refresh();
         };
 
         $(window).resize(function() {
-            $('section').height($(window).innerHeight());
+            $('section').height($(window).innerHeight() * 2);
             s.refresh();
         });
     });
 
 
 })();
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
